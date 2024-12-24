03fa92e9f6d14fc7a93169545764f4ba98506417
ayush-ojha-11 <ayushojha1512@gmail.com>
2024-12-25 01:37:38 +0530

    footer and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "./NavBar";
+import Login from "./Login";
+import Body from "./Body";
+import Profile from "./Profile";
 
 function App() {
   return (
     <>
       <BrowserRouter basename="/">
         <Routes>
-          <Route path="/" element={<div>Base Page</div>}></Route>
-          <Route path="/login" element={<div>Login Page</div>}>
-            {" "}
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
-      <NavBar />
     </>
   );
 }
